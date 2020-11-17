26935f3699c46dc43ad9e8f8cfe8a2d8a1e1c133
Arojas7 <aernestorojas@estudiante.uniajc.edu.co>
2020-11-16 22:32:16 -0500

    Registro user

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,12 +6,16 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/arojas17/parqueouniajc/middlew"
+	"github.com/arojas17/parqueouniajc/routers"
 	"github.com/rs/cors"
 )
 
 /*ControllerUSer seteo mi puerto, el Handler y pongo a escuchar al Servidor */
 func ControllerUSer() {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/registro", middlew.ChequeoBD(routers.Registro)).Methods("POST")
 
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
